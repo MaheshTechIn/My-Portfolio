6985fb90c7e98cd31fb99775fb2fd41fdb5eb151
MaheshTechIn <maheshthummaluru123@gmail.com>
2025-12-16 22:41:04 +0530

    Made changes in the Navbar Component and App.jsx

--- Portfolio/my-portfolio/src/App.jsx
+++ Portfolio/my-portfolio/src/App.jsx
@@ -6,18 +6,14 @@ import Contact from './pages/Contact';
 import { NavLink } from 'react-router-dom';
 import './Styles/Navbar.css';
 import './App.css';
-
+import Navbar from './components/Navbar';
 export default function App() {
   return (
       <div className="app-container">
 
         {/* Navigation Sticky at top */}
-        <nav className="navBar">
-
-          <NavLink to="/" className='nav-link'>Home</NavLink>
-          <NavLink to="/projects" className='nav-link'>Projects</NavLink>
-          <NavLink to="/contact" className='nav-link'>Contact</NavLink>
-        </nav>
+        <Navbar /> 
+       
 
         {/* Content below navbar */}
         <div className="content">
