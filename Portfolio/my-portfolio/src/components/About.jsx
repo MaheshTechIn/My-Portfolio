9c2f8bb710745aada1f2cface2bc5761dddbabd5
MaheshTechIn <maheshthummaluru123@gmail.com>
2025-12-18 00:58:26 +0530

    Today i made changes in components like About.jsx, Hero.jsx and skills.jsx

---
+++ Portfolio/my-portfolio/src/components/About.jsx
@@ -0,0 +1,21 @@
+export default function About(){
+    return (
+        <section className="about">
+            <h2>About Me 🙋‍♂️</h2>
+
+            <p>
+               A dedicated undergraduate pursuing a B.Tech in Computer Science and Engineering, aspiring to build a career as a Software Developer.
+               Gained 4 months of hands-on experience as a Frontend Developer Intern, strengthening skills in building responsive and user-friendly applications. Passionate about technology, problem-solving, and eager to contribute to innovative software solutions.
+            </p>
+
+            <ul className="about-list">
+                <li><strong>Education:</strong> Bachelor of Technology (B.Tech)</li>
+                <li><strong>Programming:</strong> Java, HTML, CSS, JavaScript</li>
+                <li><strong>Frameworks:</strong> React, Spring Boot</li>
+                <li><strong>Tools:</strong> Git, VS Code, Eclipse, Ubuntu</li>
+                <li><strong>Languages:</strong> English, Telugu, Hindi</li>
+            </ul>
+        </section>
+
+    )
+}
---
+++ Portfolio/my-portfolio/src/components/Skills.jsx
@@ -0,0 +1,14 @@
+export default function Skills(){
+    return (
+        <section className="skills">
+            <ul>
+                <li>Java</li>
+                <li>React</li>
+                <li>Spring Boot</li>
+                <li>JavaScript</li>
+                <li>HTML & CSS</li>
+                <li>SQL & NoSQL Databases</li>
+            </ul>
+        </section>
+    )
+}
